c9c5288ae8830e4be29ba596d92daf1790008a1a
Sébastien Pondichy <sebastien.pondichy@gmail.com>
2023-05-21 09:55:41 +0200

    fix(neovim): change mini.comment to Comment.nvim
    Remove configuration of mini.comment
    Add numToStr/Comment.nvim
    
    This fixes the error empty commentstring for terraform files when trying to set text in comment

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -18,24 +18,24 @@ return {
     },
 	},
 	-- comments
-	{
-		"echasnovski/mini.comment",
-		version = "*",
-    dependencies = {
-      "JoosepAlviste/nvim-ts-context-commentstring",
-    },
-		event = "VeryLazy",
-		opts = {
-			hooks = {
-				pre = function()
-					require("ts_context_commentstring.internal").update_commentstring({})
-				end,
-			},
-		},
-		config = function(_, opts)
-			require("mini.comment").setup(opts)
-		end,
-	},
+	-- {
+	-- 	"echasnovski/mini.comment",
+	-- 	version = "*",
+	--    dependencies = {
+	--      "JoosepAlviste/nvim-ts-context-commentstring",
+	--    },
+	-- 	event = "VeryLazy",
+	-- 	opts = {
+	-- 		hooks = {
+	-- 			pre = function()
+	-- 				require("ts_context_commentstring.internal").update_commentstring({})
+	-- 			end,
+	-- 		},
+	-- 	},
+	-- 	config = function(_, opts)
+	-- 		require("mini.comment").setup(opts)
+	-- 	end,
+	-- },
 	-- auto complete pairs
 	{
 		"echasnovski/mini.pairs",
