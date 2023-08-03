6d633955befbef27f10cac97fba23d5fa12245d9
Sébastien Pondichy <sebastien.pondichy@stib.brussels>
2023-08-03 09:32:33 +0200

    feat(lazy.nvim): only check for plugin updates once a day

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -28,8 +28,12 @@ require("lazy").setup({
 		-- version = "*", -- try installing the latest stable version for plugins that support semver
 	},
 	install = { colorscheme = { "tokyonight", "habamax" } },
-	checker = { enabled = true }, -- automatically check for plugin updates
-	performance = {
+	 -- automatically check for plugin updates once a day
+  checker = {
+    enabled = true,
+    frequency = 86400
+  },
+  performance = {
 		rtp = {
 			-- disable some rtp plugins
 			disabled_plugins = {
