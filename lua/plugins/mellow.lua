47bbed7d4005f87f2663b7920d509cc163a93e85
Sébastien Pondichy <sebastien.pondichy@gmail.com>
2024-06-30 21:46:46 +0200

    feat: disable catppuccin - add and enable mellow theme

---
+++ lua/plugins/mellow.lua
@@ -0,0 +1,14 @@
+return
+{
+  {
+    "mellow-theme/mellow.nvim",
+      lazy = false,
+      priority = 1000,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "mellow",
+    },
+  },
+}
